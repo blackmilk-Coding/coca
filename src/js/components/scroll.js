811add83d26178bce3eac2eb51a13d67684f5b5d
Илья <ilaceevic@gmail.com>
2024-12-19 21:57:51 +0500

    hero section

--- src/js/components/scroll.js
+++ src/js/components/scroll.js
@@ -1,26 +1,20 @@
 export const scrollHeader = () => {
   const header = document.querySelector('.header')
+
   let lastScroll = 0;
-  console.log(header.offsetHeight)
   window.addEventListener('scroll', function () {
     let currentScroll = this.pageYOffset
     const scrollPosition = document.documentElement.scrollTop;
-    console.log(scrollPosition);
-    if(currentScroll <= 1){
-      header.style.position = "static"
-      
-    }
-    if(currentScroll < 100){
-      header.style.position = "fixed"
-    }
     
-    if(currentScroll > 100){
-      header.style.position = "static"
-    }
+ 
+      if (currentScroll < lastScroll) {
+        console.log(lastScroll)
+        header.style.position = "fixed"
+        header.style.backgroundColor = "inherit"
+      }
+    
 
-    if (currentScroll < lastScroll) {
-      header.style.position = "fixed"
-    }
+
     lastScroll = currentScroll;
 
   });
