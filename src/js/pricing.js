2a8d510ea5d017614d56d74b3255dcd42628f186
Илья <ilaceevic@gmail.com>
2025-01-07 11:58:21 +0500

    team-page hero section

--- src/js/pricing.js
+++ src/js/pricing.js
@@ -1,7 +1,9 @@
 import '/scss/pricing.scss';
 
 // components
+
 import { useTheme } from './components/theme.js';
+import { scrollHeader } from './components/scroll.js';
 import { useBurger } from './components/burger.js';
 import { usePriceSwitcher } from './components/priceSwticher.js';
 import { useAccordion } from "./components/accordion.js";
@@ -10,3 +12,4 @@ useTheme();
 useBurger();
 usePriceSwitcher();
 useAccordion();
+scrollHeader();
--- src/js/team.js
+++ src/js/team.js
@@ -2,9 +2,16 @@ import '/scss/team.scss';
 
 // components
 import { useTheme } from './components/theme.js';
+import { scrollHeader } from './components/scroll.js';
 import { useBurger } from './components/burger.js';
 import { useAccordion } from "./components/accordion.js";
+import {
+  useTeamSlider,
+} from './components/slider.js';
+
 
 useTheme();
 useBurger();
+scrollHeader();
+useTeamSlider();
 useAccordion();
