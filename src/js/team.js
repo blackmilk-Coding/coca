32e3be0d0a5070ab61c88d823a4f7ee291c3e965
Илья <ilaceevic@gmail.com>
2025-01-07 09:49:39 +0500

    team page add

---
+++ src/js/team.js
@@ -0,0 +1,10 @@
+import '/scss/team.scss';
+
+// components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { useAccordion } from "./components/accordion.js";
+
+useTheme();
+useBurger();
+useAccordion();
