3553260c6bedadcd828780a8953608bfe425d7cb
Илья <ilaceevic@gmail.com>
2025-04-09 20:52:54 +0500

    partners slider page contacts fixed

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -5,6 +5,7 @@ import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { scrollHeader } from './components/scroll.js';
 import { useAccordion } from "./components/accordion.js";
+import { usePartnersSlider } from "./components/slider.js"
 import { usePhone } from './components/contacts/phone.js';
 import { useValidation } from "./components/contacts/formValid.js"
 
