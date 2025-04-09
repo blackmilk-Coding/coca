9f341ff2bbc52698f4faf7dc7f2c64f2a709b82d
blackmilk-Coding <milkblack664@gmail.com>
2025-04-09 21:23:18 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   root: './src/',
-  base: './src/assets',
+  base: '/assets',
   server: {
     port: 3000,
   },
