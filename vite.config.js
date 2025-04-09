d06116429f44e9259718e9f9fd14127c7e79df9a
blackmilk-Coding <milkblack664@gmail.com>
2025-04-09 21:33:29 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  publicDir: './src/',
+  publicDir: './coca/src/assets',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
