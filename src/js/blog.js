f335d353b4cf78f5d95a67716efde9e5a38b12e7
Илья <ilaceevic@gmail.com>
2025-04-13 18:48:49 +0500

    blog page

---
+++ src/js/blog.js
@@ -0,0 +1,16 @@
+import '/scss/blog.scss';
+
+
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { scrollHeader } from './components/scroll.js';
+import { useAccordion } from "./components/accordion.js";
+
+
+scrollHeader();
+useTheme();
+useBurger();
+//-----------
+
+//-----------
+useAccordion();
